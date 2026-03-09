3cfd60359a504d161434a0c61512d1f1fad07f42
Icer Liang <liangshuang@sutu.tech>
2026-03-09 14:16:28 +0900

    add GitHub Pages deployment workflow and configure Astro for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: process.env.SITE,
+  base: process.env.BASE_PATH,
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "ja",
